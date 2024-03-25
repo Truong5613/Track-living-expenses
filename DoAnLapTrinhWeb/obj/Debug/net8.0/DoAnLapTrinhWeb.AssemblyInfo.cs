67e17c0b7f17370623420fdce70ab7cb43b4c5f1
Truong5613 <143669324+Truong5613@users.noreply.github.com>
2024-03-25 23:04:36 +0700

    Deo Biet sua j nhung ma xong roi
    Chua lam 3 lop

--- DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
+++ DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0683551e771caaffc3872219eb52e6ae9306b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6511a084892f38725fbb9d2a941eaf38cf6104b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
