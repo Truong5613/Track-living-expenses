33c14c5a338201eb500335711044b95ae33bb540
Truong5613 <143669324+Truong5613@users.noreply.github.com>
2024-04-11 00:21:39 +0700

    hihi
    Them ti mau + dich cho~ progress bar

--- DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
+++ DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afaf01db338a89217d8481e81c97853270cc4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36b01412cc22d086e85ce027a92e4c545ce549d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
