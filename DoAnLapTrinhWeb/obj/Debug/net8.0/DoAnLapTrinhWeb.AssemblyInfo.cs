7ce35a11bc5563d910544740b67eacc2cc57888d
KaeseyVNK <128293584+KaeseyVNK@users.noreply.github.com>
2024-04-20 22:34:21 +0700

    +1

--- DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
+++ DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eabf744e9d336f84703caae564e63a45c8e098c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7696356106de693ddecb403f51662eaae7b2c904")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
